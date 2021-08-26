5b4011f585f2eed47d35c4158117c6f6c07ef5eb
och5351 <dhcksgo5319@naver.com>
2021-08-26 16:33:39 +0900

    code-server container exe

--- run.sh
+++ run.sh
@@ -7,14 +7,13 @@ echo -e "비밀번호 :"
 read pass
 echo -e "포트 :"
 read port
-echo -e "공유디렉터리 경로 :"
-read path
 
 sudo docker run \
 --name ${name} \
 --hostname ${honame} \
 -i -t -d \
 -p ${port}:8080 \
--v ${path}:/home/coder/project \
+-u $(id -u):$(id -g) \
+-v /home/${name}:/home/coder/project \
 -e PASSWORD=${pass} \
 codercom/code-server:latest
